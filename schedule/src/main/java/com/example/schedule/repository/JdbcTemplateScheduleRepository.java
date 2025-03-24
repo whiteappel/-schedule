1e46c1fb6d4d48fc0429ee28117755ac8f2e89d8
148june@naver.com <148june@naver.com>
2025-03-24 19:20:33 +0900

    jdbc부분 완료

--- schedule/src/main/java/com/example/schedule/repository/JdbcTemplateScheduleRepository.java
+++ schedule/src/main/java/com/example/schedule/repository/JdbcTemplateScheduleRepository.java
@@ -27,14 +27,14 @@ public class JdbcTemplateScheduleRepository implements ScheduleRepository {
     public JdbcTemplateScheduleRepository(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
-
+    //생성
     @Override
     public ScheduleResponseDto saveSchedule(Schedule schedule){
         //insert 퀴리 문자열 직접 작성안해도되게해줌 그리고 자동증가 ID
         SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
         jdbcInsert.withTableName("schedule").usingGeneratedKeyColumns("id");
 
-        //현재시간
+        //현재시간 가져옴
         LocalDateTime now = LocalDateTime.now();
 
         Map<String, Object> parameters = new HashMap<>();
@@ -42,43 +42,37 @@ public class JdbcTemplateScheduleRepository implements ScheduleRepository {
         parameters.put("password", schedule.getPassword());
         parameters.put("todo", schedule.getTodo());
         parameters.put("createday", now);
-        parameters.put("repotingday", now);//최초값은 수정일과 작성일 같으므로 이렇게 작성
+        parameters.put("reportingday", now);//최초값은 수정일과 작성일 같으므로 이렇게 작성
 
         //저장후 생성된 key값 number 타입으로 반환
         Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
 
-        return new ScheduleResponseDto(key.longValue(), schedule.getName(), schedule.getPassword(), schedule.getTodo(), schedule.getCreateday(), schedule.getReportingday());
+        return new ScheduleResponseDto(key.longValue(), schedule.getName(), schedule.getPassword(), schedule.getTodo(), now, now);
     }
-
+    //전체 조회
     @Override
     public List<ScheduleResponseDto> findAllSchedules() {
-        return jdbcTemplate.query("select * from schedule", scheduleRowMapper());
+        return jdbcTemplate.query("select * from schedule ORDER BY reportingday ", scheduleRowMapper());
     }
-
+    //일부 조회
     @Override
     public Optional<Schedule> findScheduleById(long id) {
         List<Schedule> result = jdbcTemplate.query("select * from schedule where id = ?", scheduleRowMapperV2(), id);
 
         return result.stream().findAny();
     }
-
-
+    //오류제어
     @Override
     public Schedule findScheduleByIdOrElseThrow(Long id){
         List<Schedule> result = jdbcTemplate.query("select * from schedule where id = ?", scheduleRowMapperV2(), id);
         return result.stream().findAny().orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Does not exists id=?"+id));
     }
-
+    //수정
     @Override
-    public int updateSchedule(Long id, String title, String todo){
-        return jdbcTemplate.update("update schedule " + "set title = ?, todo = ?  " + "where id = ?",title,todo,id);
+    public int updateSchedule(Long id, String name, String todo){
+        return jdbcTemplate.update("update schedule set name = ?, todo = ?, reportingday =?  where id = ?",name,todo,LocalDateTime.now(),id);
     }
-
-    @Override
-    public int updateTitle(Long id, String title){
-        return jdbcTemplate.update("update schedule set title = ? where id = ?", title, id);
-    }
-
+    //삭제
     @Override
     public int deleteSchedule(Long id) {
         return jdbcTemplate.update("delete from schedule where id = ?", id);
@@ -91,8 +85,11 @@ public class JdbcTemplateScheduleRepository implements ScheduleRepository {
             public ScheduleResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
                 return new ScheduleResponseDto(
                         rs.getLong("id"),
-                        rs.getString("title"),
-                        rs.getString("todo")
+                        rs.getString("name"),
+                        rs.getNString("password"),
+                        rs.getString("todo"),
+                        rs.getTimestamp("createday").toLocalDateTime(),
+                        rs.getTimestamp("reportingday").toLocalDateTime()
                 );
             }
 
@@ -105,9 +102,10 @@ public class JdbcTemplateScheduleRepository implements ScheduleRepository {
                 @Override
                 public Schedule mapRow(ResultSet rs, int rowNum) throws SQLException {
                     return new Schedule(
-
-                        rs.getString("title"),
-                        rs.getString("todo")
+                        rs.getString("name"),
+                        rs.getString("password"),
+                        rs.getString("todo"),
+                        rs.getTimestamp("reportingday").toLocalDateTime()
                     );
                 }
 
